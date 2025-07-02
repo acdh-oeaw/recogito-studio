8f3e3f77e7fd8c57965980f2c9e825a0cff378b2
Dalibor Pančić <dalibor.pancic@gmail.com>
2025-07-02 22:27:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ sleep 5
 echo 'Create default groups'
 yes | node /app/recogito-server/create-default-groups.js -f /app/recogito-server/config.json
 
-echo "adjust NER plugin to trigger-dev v4"
-sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
+#echo "adjust NER plugin to trigger-dev v4"
+#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
 #sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/stanfordCore.ts
 #sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/doStandfordNlp.ts
 #sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/nerToXML.ts
@@ -30,8 +30,8 @@ echo "Start the delayed command in the background"
   echo "Waiting 30 seconds before running trigger.dev deploy..."
   sleep 30
   cd /app/recogito-client
-  yes | npx trigger.dev@v4-beta dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
-# yes | npx trigger.dev@latest dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
+#  yes | npx trigger.dev@v4-beta dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
+ yes | npx trigger.dev@latest dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
 ) &
 
 echo "Client build completed"
