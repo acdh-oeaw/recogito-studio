ac60de77078b75e3c51341f72ebc603554958ba3
Dalibor Pančić <dalibor.pancic@gmail.com>
2025-07-03 09:37:47 +0200

    adjust entrypoint to selfhosted profile

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,15 @@ sleep 5
 echo 'Create default groups'
 yes | node /app/recogito-server/create-default-groups.js -f /app/recogito-server/config.json
 
-#echo "adjust NER plugin to trigger-dev v4"
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/stanfordCore.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/doStandfordNlp.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/nerToXML.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/plainTextToXML.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/uploadDocumentToRS.ts
-#sed -i 's|trigger.dev/sdk/v3|trigger.dev/sdk|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger/tasks/xmlToPlainText.ts
-
-echo "adjust to acdh trigger project"
-sed -i 's|proj_fyeypkhgyaejpiweobwq|proj_ojornoedwjbzoigcdkcn|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
-
-
-#echo "Start the delayed command in the background"
-#(
-#  echo "Waiting 30 seconds before running trigger.dev deploy..."
-#  sleep 30
-#  cd /app/recogito-client
-#  yes | npx trigger.dev@v4-beta dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
- # yes | npx trigger.dev@latest dev deploy -c /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
-#) &
+echo "Start the delayed command in the background"
+(
+  sed -i 's|proj_fyeypkhgyaejpiweobwq|proj_ojornoedwjbzoigcdkcn|g' /app/recogito-client/node_modules/@recogito/plugin-ner/src/trigger.config.ts
+  echo "Waiting 30 seconds before running trigger.dev deploy..."
+  sleep 30
+  cd  /app/recogito-client/node_modules/@recogito/plugin-ner
+  yes | npx trigger.dev@v4-beta login -a https://recogito-trigger-v4.acdh-dev.oeaw.ac.at --profile self-hosted
+  yes | npx trigger.dev@v4-beta dev --profile self-hosted deploy -c ./src/trigger.config.ts
+) &
 
 echo "Client build completed"
 node /app/recogito-client/dist/server/entry.mjs
